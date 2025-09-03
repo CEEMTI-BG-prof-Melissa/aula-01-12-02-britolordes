cd55b49fff6514454943634844a2a16ac2173a73
Gabriel <gabriel.bcunha@aluno.edu.es.gov.br>
2025-09-03 11:56:20 -0300

    Create index.php

---
+++ aula02-3tri/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Aula 02 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Gabriel";
+            $nome2="Raqueli";
+            echo "Meu nome é ".$nome1;
+            echo "Minha dupla é ".$nome2;
+        ?>
+
+    </body>
+</html> 
