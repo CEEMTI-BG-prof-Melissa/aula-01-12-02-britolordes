dbec3a52911591233fe0c07aa3f0c77b14a777ab
Gabriel <gabriel.bcunha@aluno.edu.es.gov.br>
2025-09-09 11:55:54 -0300

    Create index.php

---
+++ aula03-3tri/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Aula 03 PHP</title>
+    </head>
+    <body>
+        <?php
+            $nome1="Gabriel";
+            $nome2="Raqueli";
+
+            $n1=10;
+            $n2=2;
+            $soma=$n1+$n2;
+
+
+
+            echo "Meu nome é ".$nome1;
+            echo "<br>Minha dupla é ".$nome2;
+
+            echo "<br>Meu nome é $nome1 minha dupla é $nome2";
+
+            echo"<br><br>a soma de $n1 com $n2 e igual a $soma ";
+        ?>
+
+    </body>
+</html> 
