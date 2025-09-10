f38f047e90d59cc1f109e0208bc2673996431919
Gabriel <gabriel.bcunha@aluno.edu.es.gov.br>
2025-09-10 11:54:41 -0300

    Update index.php

--- aula03-3tri/index.php
+++ aula03-3tri/index.php
@@ -15,10 +15,13 @@
 
             echo "Meu nome é ".$nome1;
             echo "<br>Minha dupla é ".$nome2;
-
             echo "<br>Meu nome é $nome1 minha dupla é $nome2";
 
             echo"<br><br>a soma de $n1 com $n2 e igual a $soma ";
+            echo"<br><br> A subtração dos numeros $n1 e $n2 é igual a " .($n1-$n2);
+            echo"<br><br> A multiplicação dos numeros $n1 e $n2 é igual a " .($n1*$n2);
+            echo"<br><br> A divisão dos numeros $n1 e $n2 é igual a " .($n1/$n2);
+            echo"<br><br> O resto divisão dos numeros $n1 e $n2 é igual a " .($n1%$n2);
         ?>
 
     </body>
