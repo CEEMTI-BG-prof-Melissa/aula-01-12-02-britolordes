b14272f69e09e4eb6084612b13f2a7a10a475ece
Gabriel <gabriel.bcunha@aluno.edu.es.gov.br>
2025-09-30 11:47:05 -0300

    Create index.php

---
+++ aula05-3tri/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title> Aula 05 PHP</title>
+    </head>
+    <body>
+        <?php
+            $n1=2 ;
+            $n2=$_GET["x"] ;
+
+            echo "Os valores são $n1 e $n2. <br>";
+            echo "<br>O valor absoluto de $n1 é " .abs($n1);
+            echo "<br><br>O número $n2 <sup>$n1</sup> é igual a: " .pow($n2, $n1);
+            echo "<br><br>A raiz quadrada de $n2 é igual a: " .sqrt($n2);
+            echo "<br><br>Arredondamento de $n2 é igual a: " .round($n2);
+            echo "<br><br>O arredondamento para cima de $n2 é igual a: " .ceil($n2);
+            echo "<br><br> O arredondamento para baixo de $n2 é igual a: " .floor($n2);
+
+            echo"<br><br><br>";
+            echo"A parte inteira de $n2 e igual a " .intval($n2);
+            echo"<br><br>";
+            echo"Um pirulito custa R$" .number_format($n1, 2);
+
+        ?>
+    </body>
+</html>
